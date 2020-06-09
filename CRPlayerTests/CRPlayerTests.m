3eca9034439108eb2b77956f1fa55d6ac57f6962
gcr <kangomake@163.com>
2020-06-09 18:11:57 +0800

    init_01

---
+++ CRPlayerTests/CRPlayerTests.m
@@ -0,0 +1,37 @@
+//
+//  CRPlayerTests.m
+//  CRPlayerTests
+//
+//  Created by appleDeveloper on 2020/6/9.
+//  Copyright © 2020 appleDeveloper. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CRPlayerTests : XCTestCase
+
+@end
+
+@implementation CRPlayerTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
